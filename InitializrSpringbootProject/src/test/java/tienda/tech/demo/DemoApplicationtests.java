8cca2acaae79cc0e5e1215a61163a79849ea2766
Steph <stepharg1963@gmail.com>
2025-10-04 15:14:15 -0600

    ACtualizacion proyecto

---
+++ InitializrSpringbootProject/src/test/java/tienda/tech/demo/DemoApplicationtests.java
@@ -0,0 +1,9 @@
+package tienda.tech.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class DemoApplicationtests {
+    @Test
+	void contextLoads() {
+	}
+}
