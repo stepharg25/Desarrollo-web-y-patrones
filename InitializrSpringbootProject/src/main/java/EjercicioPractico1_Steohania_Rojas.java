10cd8f20f57b084ece0e1d9cc08015c2e8d1bbda
Steph <stepharg1963@gmail.com>
2025-11-26 17:53:31 -0600

    configs

---
+++ InitializrSpringbootProject/src/main/java/EjercicioPractico1_Steohania_Rojas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springboot/CfgProperties.java to edit this template
+ */
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ *
+ * @author XT
+ */
+@ConfigurationProperties(prefix = "prefix")
+public class EjercicioPractico1_Steohania_Rojas {
+    
+}
